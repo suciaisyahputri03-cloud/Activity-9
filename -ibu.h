aa5599d4a07f9a2f336454923a02aab40715d0ff
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-12 19:47:14 +0700

    memperbaiki eror

--- -ibu.h
+++ -ibu.h
@@ -1,29 +1,45 @@
 #ifndef IBU_H
-#define IBU-H
-#include <vektor>
+#define IBU_H
+
+#include <iostream>
+#include <string>
+#include "anak.h"
+using namespace std;
+
+#include <vector>
 
 class ibu {
 public:
     string nama;
     vector<anak*> daftar_anak;
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
 
-    ibu(string pNama) :nama(pNama) {
-        cout << "Ibu \ "" << nama << "\" ada\n";
-    }
     ~ibu() {
-        cout << "ibu \"" << nama << "\" tidak ada\n";
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
-    void tambah Anakk(anak*);
+
+    void tambahAnak(anak*);
     void cetakAnak();
 };
-void ibu::cetakAnak(){
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
     cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
-    /*for(auto& a: daftar_anak){
+
+    /*for (auto& a : daftar_anak) {
         cout << a->nama << "\n";
     }*/
-   for (int i = 0; i < daftar_anak.size(); i++){
+
+    for (int i = 0; i < daftar_anak.size(); i++) {
         cout << daftar_anak[i]->nama << endl;
-   }
+    }
+
     cout << endl;
 }
+
 #endif
